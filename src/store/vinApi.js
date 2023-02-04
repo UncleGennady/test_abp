641fc1432b1863aa990ccc066c5ba044fced2078
Aleksandr Protasenko <96978077+UncleGennady@users.noreply.github.com>
2023-02-04 15:06:02 +0200

    app prepared for deployment

--- src/store/vinApi.js
+++ src/store/vinApi.js
@@ -2,7 +2,7 @@ import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react';
 
 export const vinApi = createApi({
     reducerPath: 'vinApi',
-    baseQuery: fetchBaseQuery({ baseUrl: 'https://vpic.nhtsa.dot.gov/api/vehicles/' }),
+    baseQuery: fetchBaseQuery({ baseUrl: process.env.REACT_APP_API_URL }),
     endpoints: (builder) => ({
         getDecodeVin: builder.query({
             query: (vin) => `decodevin/${vin}?format=json`,
